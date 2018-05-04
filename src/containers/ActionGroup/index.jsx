da551af8eae0ad55ae85474c1cb838c732600f6e
leezng <leezng@outlook.com>
2018-05-04 23:05:42 +0800

    fixed: 1.歌曲已存在列表中则不能重复添加 2.歌曲正在播放时，再次点击播放无效

--- src/containers/ActionGroup/index.jsx
+++ src/containers/ActionGroup/index.jsx
@@ -11,6 +11,11 @@ import {
   removeFromPlaylist
 } from 'src/actions'
 
+const mapStateToProps = (state, ownProps) => ({
+  playlist: state.playlist,
+  playing: state.playing
+})
+
 class ActionGroup extends Component {
   static propTypes = {
     // actions为描述操作组的配置[StringArray], 可选: add, remove, play
@@ -24,19 +29,36 @@ class ActionGroup extends Component {
     song: {}
   }
 
+  // 新的歌曲不存在队列中, 才进行添加
+  addToPlaylist = song => {
+    const { dispatch, playlist } = this.props
+    const exist = playlist.findIndex(item => item.id === song.id) !== -1
+    if (!exist) dispatch(addToPlaylist(song))
+    else console.log('歌曲已存在队列中', song)
+  }
+
   switcher = (action, song) => {
-    const { dispatch } = this.props
+    const { dispatch, playing } = this.props
     switch (action) {
+      // 播放
       case 'play':
         return {
           icon: 'caret-right',
-          onClick: () => dispatch(addToPlaylist(song)) && dispatch(setPlaying(song))
+          onClick: () => {
+            this.addToPlaylist(song)
+            // 当前没有播放歌曲/正在播放的歌曲ID不一致, 则更新正在播放
+            if (!playing || playing.id !== song.id) {
+              dispatch(setPlaying(song))
+            }
+          }
         }
+      // 添加到播放队列
       case 'add':
         return {
           icon: 'plus-circle',
-          onClick: () => dispatch(addToPlaylist(song))
+          onClick: () => this.addToPlaylist(song)
         }
+      // 从播放队列中移除
       case 'remove':
         return {
           icon: 'delete',
@@ -56,4 +78,4 @@ class ActionGroup extends Component {
   }
 }
 
-export default connect()(ActionGroup)
+export default connect(mapStateToProps)(ActionGroup)
