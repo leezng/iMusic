0ebdde9b19b5a9a08bbb176ec2f89f18e2ceee63
leezng <leezng@outlook.com>
2018-07-19 19:37:00 +0800

    简单模拟用户切换时的信息通信

--- src/renderer/reducers/user.js
+++ src/renderer/reducers/user.js
@@ -1,3 +1,5 @@
+import { ipcRenderer } from 'electron'
+
 // 本地用户
 const local = {
   name: 'LOCAL',
@@ -6,22 +8,26 @@ const local = {
 }
 
 const user = (state = local, action) => {
+  let result
   switch (action.type) {
     // 登录为本地用户
     case 'SET_LOCAL_USER':
-      return local
+      result = local
+      break
     // 登录为某个在线用户
     case 'SET_USER':
       let meta = {...action}
       delete meta.type
-      return meta
+      result = meta
+      break
     // 注意: 以下操作不可改动status, isLocal等所有基础属性
     // 设置用户歌单
     case 'SET_USER_SONGLIST':
-      return {
+      result = {
         ...state,
         ...{songlist: action.songlist}
       }
+      break
     // 设置用户歌单详情
     case 'SET_USER_SONGLIST_DETAIL':
       let data = {...state}
@@ -30,10 +36,18 @@ const user = (state = local, action) => {
         if (item.id === action.id) item.tracks = action.tracks
         return item
       })
-      return data
+      result = data
+      break
     default:
-      return state
+      result = state
   }
+
+  const oldUser = state.profile && state.profile.userId
+  const newUser = result.profile && result.profile.userId
+  if (newUser !== oldUser) {
+    ipcRenderer.send('set-user', newUser)
+  }
+  return result
 }
 
 export default user
