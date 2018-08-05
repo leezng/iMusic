96b852806c31d53e1fbadb03944c8dcdcf9db72c
leezng <leezng@outlook.com>
2018-08-05 15:22:22 +0800

    完善用户数据目录相关操作

--- src/main/preference.js
+++ src/main/preference.js
@@ -15,22 +15,22 @@ let user = {
 }
 
 /**
- * 同步创建目录
+ * 同步创建目录, 支持多层级
  * @param  {String} dirname 目录路径
  * @return {Boolean}        创建是否成功
  */
-function mkdirsSync (dirname) {
+function mkdir (dirname) {
   try {
     if (fs.existsSync(dirname)) {
       return true
     } else {
-      if (mkdirsSync(path.dirname(dirname))) {
+      if (mkdir(path.dirname(dirname))) {
         fs.mkdirSync(dirname)
         return true
       }
     }
   } catch (err) {
-    console.error('创建目录失败', err)
+    console.error('create dir failed', err)
     return false
   }
 }
@@ -38,76 +38,69 @@ function mkdirsSync (dirname) {
 /**
  * 确保用户目录存在
  * @param  {String} id 用户ID, 未登录时为global
+ * @return {String} 用户目录文件夹
  */
 function ensureUserDir (id) {
-  const pa = path.join(app.getPath('userData'), `./user/${id}`)
+  const userPath = path.join(app.getPath('userData'), `./user/${id}`)
   try {
     fs.accessSync(pa, fs.F_OK)
   } catch (err) {
-    mkdirsSync(pa)
+    mkdir(pa)
   }
-  // return pa
-
-  try {
-    const file = path.join(pa, './config.json')
-    // fs.readFile(file, (err, data) => {
-    //   if (!err) {
-    //     let obj = data ? JSON.parse(data.toString()) : {}
-    //     obj.proxy = 'http://proxy'
-    //     let buffer = Buffer.from(JSON.stringify(obj))
-    //     fs.writeFileSync(file, buffer)
-    //   } else {
-    //     fs.writeFileSync(file, Buffer.from(JSON.stringify({})))
-    //   }
-    // })
-  } catch (err) {
-    console.log('写入出错', err)
-  }
-  return pa
+  return userPath
 }
 
 function updateUserConfig (key, value) {
+  const file = path.join(user.path, './config.json')
+  const result = readFile(file) || {}
+  obj[key] = value
+  writeFile(file, obj)
+}
+
+/**
+ * 写入文件
+ * @param  {String} file 文件路径
+ * @param  {Object|String} data 写入的内容
+ * @param  {Boolean} append 是否为追加模式, 默认false为覆盖原文件
+ */
+function writeFile (file, data, append = false, encoding = 'utf8') {
   try {
-    const file = path.join(user.path, './config.json')
+    const buffer = Buffer.from(JSON.stringify(data))
+    fs.writeFileSync(file, buffer, {
+      encoding,
+      flag
+    })
+  } catch (err) {
+    console.log('write file failed', err)
+  }
+}
+
+/**
+ * 读取文件
+ * @param  {String} file 文件路径
+ * @return {Object|String|Undefined}
+ */
+function readFile (file) {
+  try {
     const result = fs.readFileSync(file)
-    // file已存在, 则更新file
-    let obj = JSON.parse(result.toString())
-    obj.proxy = 'http://proxy'
-    const buffer = Buffer.from(JSON.stringify(obj))
-    fs.writeFileSync(file, buffer)
+    return JSON.parse(result.toString())
   } catch (err) {
-
+    console.warn('user config not exist', err)
   }
 }
 
-function writeFile (file, data) {
-  // file不存在
-  try {
-    const buffer = Buffer.from(JSON.stringify(data))
-    fs.writeFileSync(file, buffer)
-  } catch (err) {
-    console.log('failed to write in new file', err)
-  }
-}
-
-function readFile (path) {
-  try {
-    const buffer = Buffer.from(JSON.stringify(data))
-    fs.writeFileSync(file, buffer)
-  } catch (err) {
-    console.warn('user config not exist')
-    return {}
-  }
-}
-
+/**
+ * 监听登录用户是否变化
+ */
 ipcMain.on('set-user', (event, id) => {
   const userId = id || LOCAL_USER_ID
   if (userId === user.id) return
   const userPath = ensureUserDir(userId)
+  const userConfigFile = path.join(userPath, './config.json')
   user = {
     id: userId,
     path: userPath
-    config: readFileSync(path.join(userPath, './config.json'))
+    config: readFile(userConfigFile) || {}
   }
 })
 
