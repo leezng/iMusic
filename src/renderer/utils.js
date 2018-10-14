3911d49167da272906a63895dc3e88e19da31442
leezng <leezng@outlook.com>
2018-10-14 16:16:29 +0800

    前端播放器增加播放模式切换功能

--- src/renderer/utils.js
+++ src/renderer/utils.js
@@ -12,6 +12,17 @@ export function getObjectValue (data, path) {
 }
 
 /**
+ * 得到一个两数之间的随机整数，包括两个数在内
+ * @param  {Number} min
+ * @param  {Number} max
+ */
+export function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+/**
  * 设置cookie
  * @param {String} name  键名
  * @param {String} value 键值
